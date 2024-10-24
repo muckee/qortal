2629e5f2e82b7fc47696e54684f37f35cd4c2bc6
kennycud <kennycud@protonmail.com>
2024-10-23 17:39:48 -0700

    broader exception handling and added verbosity to the logging to 4 more threads

--- src/main/java/org/qortal/controller/repository/BlockArchiver.java
+++ src/main/java/org/qortal/controller/repository/BlockArchiver.java
@@ -45,71 +45,72 @@ public class BlockArchiver implements Runnable {
 			LOGGER.info("Starting block archiver from height {}...", startHeight);
 
 			while (!Controller.isStopping()) {
-				repository.discardChanges();
-
-				Thread.sleep(Settings.getInstance().getArchiveInterval());
-
-				BlockData chainTip = Controller.getInstance().getChainTip();
-				if (chainTip == null || NTP.getTime() == null) {
-					continue;
-				}
-
-				// Don't even attempt if we're mid-sync as our repository requests will be delayed for ages
-				if (Synchronizer.getInstance().isSynchronizing()) {
-					continue;
-				}
-
-				// Don't attempt to archive if we're not synced yet
-				final Long minLatestBlockTimestamp = Controller.getMinimumLatestBlockTimestamp();
-				if (minLatestBlockTimestamp == null || chainTip.getTimestamp() < minLatestBlockTimestamp) {
-					continue;
-				}
-
-
-				// Build cache of blocks
 				try {
-					final int maximumArchiveHeight = BlockArchiveWriter.getMaxArchiveHeight(repository);
-					BlockArchiveWriter writer = new BlockArchiveWriter(startHeight, maximumArchiveHeight, repository);
-					BlockArchiveWriter.BlockArchiveWriteResult result = writer.write();
-					switch (result) {
-						case OK:
-							// Increment block archive height
-							startHeight += writer.getWrittenCount();
-							repository.getBlockArchiveRepository().setBlockArchiveHeight(startHeight);
-							repository.saveChanges();
-							break;
+					repository.discardChanges();
 
-						case STOPPING:
-							return;
+					Thread.sleep(Settings.getInstance().getArchiveInterval());
 
-							// We've reached the limit of the blocks we can archive
-							// Sleep for a while to allow more to become available
-						case NOT_ENOUGH_BLOCKS:
-							// We didn't reach our file size target, so that must mean that we don't have enough blocks
-							// yet or something went wrong. Sleep for a while and then try again.
-							repository.discardChanges();
-							Thread.sleep(60 * 60 * 1000L); // 1 hour
-							break;
-
-						case BLOCK_NOT_FOUND:
-							// We tried to archive a block that didn't exist. This is a major failure and likely means
-							// that a bootstrap or re-sync is needed. Try again every minute until then.
-							LOGGER.info("Error: block not found when building archive. If this error persists, " +
-									"a bootstrap or re-sync may be needed.");
-							repository.discardChanges();
-							Thread.sleep( 60 * 1000L); // 1 minute
-							break;
+					BlockData chainTip = Controller.getInstance().getChainTip();
+					if (chainTip == null || NTP.getTime() == null) {
+						continue;
 					}
 
-				} catch (IOException | TransformationException e) {
-					LOGGER.info("Caught exception when creating block cache", e);
-				}
+					// Don't even attempt if we're mid-sync as our repository requests will be delayed for ages
+					if (Synchronizer.getInstance().isSynchronizing()) {
+						continue;
+					}
 
+					// Don't attempt to archive if we're not synced yet
+					final Long minLatestBlockTimestamp = Controller.getMinimumLatestBlockTimestamp();
+					if (minLatestBlockTimestamp == null || chainTip.getTimestamp() < minLatestBlockTimestamp) {
+						continue;
+					}
+
+
+					// Build cache of blocks
+					try {
+						final int maximumArchiveHeight = BlockArchiveWriter.getMaxArchiveHeight(repository);
+						BlockArchiveWriter writer = new BlockArchiveWriter(startHeight, maximumArchiveHeight, repository);
+						BlockArchiveWriter.BlockArchiveWriteResult result = writer.write();
+						switch (result) {
+							case OK:
+								// Increment block archive height
+								startHeight += writer.getWrittenCount();
+								repository.getBlockArchiveRepository().setBlockArchiveHeight(startHeight);
+								repository.saveChanges();
+								break;
+
+							case STOPPING:
+								return;
+
+								// We've reached the limit of the blocks we can archive
+								// Sleep for a while to allow more to become available
+							case NOT_ENOUGH_BLOCKS:
+								// We didn't reach our file size target, so that must mean that we don't have enough blocks
+								// yet or something went wrong. Sleep for a while and then try again.
+								repository.discardChanges();
+								Thread.sleep(60 * 60 * 1000L); // 1 hour
+								break;
+
+							case BLOCK_NOT_FOUND:
+								// We tried to archive a block that didn't exist. This is a major failure and likely means
+								// that a bootstrap or re-sync is needed. Try again every minute until then.
+								LOGGER.info("Error: block not found when building archive. If this error persists, " +
+										"a bootstrap or re-sync may be needed.");
+								repository.discardChanges();
+								Thread.sleep( 60 * 1000L); // 1 minute
+								break;
+						}
+
+					} catch (IOException | TransformationException e) {
+						LOGGER.info("Caught exception when creating block cache", e);
+					}
+				} catch (Exception e) {
+					LOGGER.warn("Block Archiving stopped working. Trying again. Report this error immediately to the developers.", e);
+				}
 			}
-		} catch (DataException e) {
-			LOGGER.info("Caught exception when creating block cache", e);
-		} catch (InterruptedException e) {
-			// Do nothing
+		} catch (Exception e) {
+			LOGGER.error("Block Archiving is not working! Not trying again. Restart ASAP. Report this error immediately to the developers.", e);
 		}
 
 	}
