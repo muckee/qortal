ecf8b48aab32616466810453c8b75e2c08ac8104
kennycud <kennycud@protonmail.com>
2026-02-09 14:18:12 -0800

    reverting the latest change to the PublicKeyAccount constructor, this was made under incorrect pretenses

--- src/main/java/org/qortal/account/PublicKeyAccount.java
+++ src/main/java/org/qortal/account/PublicKeyAccount.java
@@ -1,41 +1,17 @@
 package org.qortal.account;
 
-import org.bouncycastle.crypto.generators.Ed25519KeyPairGenerator;
-import org.bouncycastle.crypto.params.Ed25519KeyGenerationParameters;
 import org.bouncycastle.crypto.params.Ed25519PublicKeyParameters;
 import org.qortal.crypto.Crypto;
 import org.qortal.data.account.AccountData;
 import org.qortal.repository.Repository;
-
-import java.security.SecureRandom;
 
 public class PublicKeyAccount extends Account {
 
 	protected final byte[] publicKey;
 	protected final Ed25519PublicKeyParameters edPublicKeyParams;
 
-	/** <p>Constructor for generating a PublicKeyAccount</p>
-	 *
-	 * @param repository Block Chain
-	 * @param publicKey 32 byte Public Key
-	 * @since v4.7.3
-	 */
 	public PublicKeyAccount(Repository repository, byte[] publicKey) {
-		super(repository, Crypto.toAddress(publicKey));
-
-		Ed25519PublicKeyParameters t = null;
-		try {
-			t = new Ed25519PublicKeyParameters(publicKey, 0);
-		} catch (Exception e) {
-			var gen = new Ed25519KeyPairGenerator();
-			gen.init(new Ed25519KeyGenerationParameters(new SecureRandom()));
-			var keyPair = gen.generateKeyPair();
-			t = (Ed25519PublicKeyParameters) keyPair.getPublic();
-		} finally {
-			this.edPublicKeyParams = t;
-		}
-
-		this.publicKey = publicKey;
+		this(repository, new Ed25519PublicKeyParameters(publicKey, 0));
 	}
 
 	protected PublicKeyAccount(Repository repository, Ed25519PublicKeyParameters edPublicKeyParams) {
