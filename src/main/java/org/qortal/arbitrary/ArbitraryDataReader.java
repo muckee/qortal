8c974c2aaa350127ecf7b9adeebeb7cecb6d0190
PhilReact <philliplangmartinez@gmail.com>
2026-02-15 00:21:27 +0200

    change sleepMs to 100

--- src/main/java/org/qortal/arbitrary/ArbitraryDataReader.java
+++ src/main/java/org/qortal/arbitrary/ArbitraryDataReader.java
@@ -202,7 +202,7 @@ public class ArbitraryDataReader {
                 // Wait for it to complete (up to 60 seconds)
                 int maxWaitMs = 60000;
                 int waitedMs = 0;
-                int sleepMs = 500;
+                int sleepMs = 100;
                 
                 while (waitedMs < maxWaitMs) {
                     try {
