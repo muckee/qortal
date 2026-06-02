31f9565e000c19b0eae20211495402f8fb66cae6
Nic <gitnub.6q580@simplelogin.com>
2026-06-02 10:44:26 +0200

    Add formatBytes function in StringUtils

--- src/main/java/org/qortal/utils/StringUtils.java
+++ src/main/java/org/qortal/utils/StringUtils.java
@@ -10,4 +10,23 @@ public class StringUtils {
 
         return sanitized;
     }
+
+    /**
+     * Format a byte count into a human-readable string using binary units (KB, MB, GB, ...).
+     *
+     * @param bytes the number of bytes
+     * @return a string such as "51.6 GB" or "512 bytes"
+     */
+    public static String formatBytes(long bytes) {
+        if (bytes < 1024) {
+            return bytes + " bytes";
+        }
+
+        String[] units = { "KB", "MB", "GB", "TB", "PB", "EB" };
+        int exponent = (int) (Math.log(bytes) / Math.log(1024));
+        exponent = Math.min(exponent, units.length); // guard against overflow beyond EB
+
+        double value = bytes / Math.pow(1024, exponent);
+        return String.format("%.2f %s", value, units[exponent - 1]);
+    }
 }
